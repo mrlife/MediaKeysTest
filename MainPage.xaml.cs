3fdceee2d2f3dbd0cb1adaf17fc33df1051c7fb9
mrlife <213164+mrlife@users.noreply.github.com>
2024-03-17 12:03:11 -0400

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MediaKeysTest;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿if (typeof navigator.requestMediaKeySystemAccess !== "undefined") {
+	alert("navigator.requestMediaKeySystemAccess exists");
+}
+else {
+	alert("navigator.requestMediaKeySystemAccess does not exist");
+}
+
+if (typeof window.MediaKeys !== "undefined") {
+	alert("window.MediaKeys exists");
+}
+else {
+	alert("window.MediaKeys does not exist");
+}
